fe964bfcb047d5c023099f7671c1ea1382c1cbdd
aront <aront@DESKTOP-JP3EPFR>
2024-09-05 16:10:54 +0200

    Add project files.

---
+++ variant-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace variant_1
+{
+    // Klass som hanterar matrislogik
+    internal class Matrix
+    {
+        private int size;
+
+        // Konstruktor som tar matrisens storlek
+        public Matrix(int size)
+        {
+            this.size = size;
+        }
+
+        // Metod som skriver ut matrisen
+        public void PrintMatrix()
+        {
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= size; j++)
+                {
+                    Console.Write((i * j).ToString().PadRight(4));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    // Huvudklass med Main-metoden
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läser in storleken på matrisen från användaren
+            Console.Write("Ange storleken på matrisen: ");
+            int size = int.Parse(Console.ReadLine());
+
+            // Skapar en ny matris med användarens storlek
+            Matrix matrix = new Matrix(size);
+
+            // Skriver ut matrisen
+            matrix.PrintMatrix();
+        }
+    }
+}
